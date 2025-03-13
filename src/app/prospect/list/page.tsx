0fbbd4b8b1818fcd8668c579407e2ed66d912d18
ricardoaot <ricardoaot@gmail.com>
2025-03-12 20:34:29 -0500

    fixed Eslint issues

--- src/app/prospect/list/page.tsx
+++ src/app/prospect/list/page.tsx
@@ -1,6 +1,5 @@
 import ProspectComponent from "@/components/ProspectList/ProspectComponent";
 import {getProspects} from "@/components/ProspectList/ProspectListQueries";
-import { Suspense } from "react";
 
 export default async function OnboardingForm() {
 
@@ -8,9 +7,7 @@ export default async function OnboardingForm() {
 
   return (
     <div style={{ maxWidth: 1200, margin: 'auto' }}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProspectComponent prospectList={prospects} />
-      </Suspense>
+      <ProspectComponent prospectList={prospects} />
     </div>
   );
 }
